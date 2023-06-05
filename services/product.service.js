9aad72afc7dca3a769ded4ea83051bba67a710cf
MarianaSanchezGonzalez03 <mari98san33@gmail.com>
2023-06-05 03:58:08 -0600

    Crear, editar y eliminar

--- services/product.service.js
+++ services/product.service.js
@@ -1,37 +1,56 @@
-const {faker } = require ('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
 
-class ProductsService{
-  constructor (){
-    this.products =[];
+class ProductsService {
+  constructor() {
+    this.products = [];
     this.generate();
   }
 
-  generate(){
+  generate() {
     const limit = 100;
-    for (let index = 0; index < limit; index++){
-    this.products.push({
-      id: faker.datatype.uuid(),
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  }
-  create(){
-
-  }
-  find(){
-    return this.products;
+    for (let index = 0; index < limit; index++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl(),
+      });
     }
-  findOne(id ){
+  }
+  create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data
+    }
+    this.products.push(newProduct);
+    return newProduct;
+  }
+  find() {
+    return this.products;
+  }
+  findOne(id) {
     return this.products.find(item => item.id === id);
 
   }
-  update(){
-
+  update(id, changes) {
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1){
+      throw new Error('product not found');
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
   }
-  delete(){
-
+  delete(id) {
+      const index = this.products.findIndex(item => item.id === id);
+      if( index === -1){
+        throw new  Error('product not found');
+      }
+      this.products.splice(index, 1);
+      return { id };
   }
 
 }
